5f687038f3e0046f86488519b2700f2fa53f55e1
rishavchanda <rishavchanda0@gmail.com>
2023-06-03 22:59:46 +0530

    Initial Commit

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit -<code>srce/App.js</code>
+          Edit <code>src/App.js</code>
           and save to reload.
         </p>
         <p>{`Server is running on ${process.env.REACT_APP_SERVER_URL}`}</p>
