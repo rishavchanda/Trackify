c29c50f1f38454cd7d243ccee5b330a7e50c8c62
rishavchanda <rishavchanda0@gmail.com>
2023-06-04 20:02:13 +0530

    added routes in index

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
+import authRoutes from "./routes/auth.js";
+import adminRoutes from "./routes/admins.js";
 
 const app = express();
 dotenv.config();
@@ -10,39 +12,44 @@ dotenv.config();
 /** Middlewares */
 app.use(express.json());
 const corsConfig = {
-    credentials: true,
-    origin: true,
+  credentials: true,
+  origin: true,
 };
 app.use(cors(corsConfig));
 app.use(morgan("tiny"));
 
-const port = process.env.PORT || 8700;
+const port = process.env.PORT || 8800;
 
 const connect = () => {
-    mongoose.set("strictQuery", true);
-    mongoose
-        .connect(process.env.MONGO_URL)
-        .then(() => {
-            console.log("MongoDB connected");
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log("MongoDB connected");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
-app.use((err, req, res) => {
-    const status = err.status || 500;
-    const message = err.message || "Something went wrong";
-    return res.status(status).json({
-        success: false,
-        status,
-        message,
-    });
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
 });
 
 app.use(express.json());
 
 app.listen(port, () => {
-    console.log("Connected");
-    connect();
+  console.log("Connected");
+  connect();
 });
