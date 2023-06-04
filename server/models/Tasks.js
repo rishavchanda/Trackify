b1716d13409b5336f8a0640ad38ab38c922f7eba
rishavchanda <rishavchanda0@gmail.com>
2023-06-04 19:58:03 +0530

    Made user and task models

---
+++ server/models/Tasks.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const TasksSchema = new mongoose.Schema(
+  {
+    task_description: { type: String, required: true },
+    task_type: { type: String, required: true, default: "work" },
+    start_time: { type: Date, required: true, default: Date.now() },
+    time_taken: { type: Number, required: true, default: 0 }
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Tasks", TasksSchema);
---
+++ server/models/User.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: false,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      default: null,
+    },
+    role: {
+      type: String,
+      default: "admin",
+    },
+    contact_number: {
+      type: String,
+      default: null,
+    },
+    department: {
+      type: String,
+      default: null,
+    },
+    joining_date: {
+      type: Date,
+      default: null,
+    },
+    employees: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "User",
+      default: [],
+    },
+    tasks: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "Tasks",
+      default: [],
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("User", UserSchema);
