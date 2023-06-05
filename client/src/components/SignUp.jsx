a1e76b53bc39a40a168134a52ffe89a2d79c8c0d
rishavchanda <rishavchanda0@gmail.com>
2023-06-05 11:24:13 +0530

    Made form data validation

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import {
   EmailRounded,
@@ -53,6 +53,9 @@ const OutlinedInput = styled.div`
   display: flex;
   align-items: center;
   gap: 12px;
+  &:focus-within {
+    border-color: ${({ theme }) => theme.primary};
+  }
 `;
 
 const Input = styled.input`
@@ -62,9 +65,14 @@ const Input = styled.input`
   border: none;
   background-color: transparent;
   color: ${({ theme }) => theme.text_secondary};
-  &:focus ${OutlinedInput} {
-    border: 1px solid ${({ theme }) => theme.primary};
+  &:focus {
+    outline: none;
   }
+`;
+
+const Error = styled.p`
+  font-size: 12px;
+  margin: 0px 4px;
 `;
 
 const Button = styled.button`
@@ -82,6 +90,16 @@ const Button = styled.button`
   &:hover {
     background-color: ${({ theme }) => theme.primary_hover};
   }
+  ${({ buttonDisabled }) =>
+    buttonDisabled &&
+    `
+    background-color: #ccc;
+    color: #fff;
+    cursor: not-allowed;
+    &:hover {
+      background-color: #ccc;
+    }
+  `}
 `;
 
 const Text = styled.p`
@@ -99,7 +117,136 @@ const TextButton = styled.span`
 `;
 
 const SignUp = () => {
+  // hooks
   const [showPassword, setShowPassword] = useState(false);
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+  const [errorMessage, setErrorMessage] = useState({
+    username: "",
+    email: "",
+    password: ""
+  }); // error message for validation checks.
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: ""
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    // validation checks
+    if (name === "email") {
+      // Email validation regex pattern
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+      if (!value) {
+        setButtonDisabled(true);
+      }
+
+      if (value && !emailRegex.test(value)) {
+        setErrorMessage({
+          ...errorMessage,
+          email: "Enter correct email format"
+        });
+        setButtonDisabled(true);
+      } else {
+        setErrorMessage({
+          ...errorMessage,
+          email: ""
+        });
+      }
+    }
+
+    if (name === "password") {
+      if (!value) {
+        setButtonDisabled(true);
+      }
+      // Password validation regex pattern
+      if (value && value.length < 8) {
+        setErrorMessage({
+          ...errorMessage,
+          password: "Password must be atleast 8 characters long!"
+        });
+        setButtonDisabled(true);
+      } else if (value && value.length > 16) {
+        setErrorMessage({
+          ...errorMessage,
+          password: "Password must be less than 16 characters long!"
+        });
+        setButtonDisabled(true);
+      } else if (
+        value &&
+        (!value.match(/[a-z]/g) ||
+          !value.match(/[A-Z]/g) ||
+          !value.match(/[0-9]/g) ||
+          !value.match(/[^a-zA-Z\d]/g))
+      ) {
+        setErrorMessage({
+          ...errorMessage,
+          password:
+            "Password must contain atleast one lowercase, uppercase, number and special character!"
+        });
+        setButtonDisabled(true);
+      } else {
+        setErrorMessage({
+          ...errorMessage,
+          password: ""
+        });
+      }
+    }
+
+    if (name === "username") {
+      if (!value) {
+        setButtonDisabled(true);
+      }
+      // Username validation regex pattern
+      const usernameRegex = /^[A-Za-z0-9\s]+$/;
+
+      if (value && !usernameRegex.test(value)) {
+        setErrorMessage({
+          ...errorMessage,
+          username: "Username must contain only letters, numbers and spaces"
+        });
+        setButtonDisabled(true);
+      } else {
+        setErrorMessage({
+          ...errorMessage,
+          username: ""
+        });
+      }
+    }
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  useEffect(() => {
+    // If there is no error message and all the fields are filled, then enable the button
+    if (
+      !errorMessage.username &&
+      !errorMessage.email &&
+      !errorMessage.password &&
+      formData.username &&
+      formData.email &&
+      formData.password
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [errorMessage, formData]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // If there is no error message, then submit the form
+    if (!buttonDisabled) {
+      console.log(formData);
+    }
+  };
+
   return (
     <Container data-testid="signup">
       <Wrapper>
@@ -107,17 +254,43 @@ const SignUp = () => {
         <Form>
           <OutlinedInput>
             <PersonRounded />
-            <Input placeholder="Username" />
+            <Input
+              placeholder="Username"
+              name="username"
+              value={formData.username}
+              onChange={handleInputChange}
+              maxLength={16}
+            />
           </OutlinedInput>
+          {
+            // Show error message if there is one
+            errorMessage?.username && (
+              <Error style={{ color: "red" }}>{errorMessage.username}</Error>
+            )
+          }
           <OutlinedInput>
             <EmailRounded />
-            <Input placeholder="Email" />
+            <Input
+              placeholder="Email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
           </OutlinedInput>
+          {
+            // Show error message if there is one
+            errorMessage?.email && (
+              <Error style={{ color: "red" }}>{errorMessage.email}</Error>
+            )
+          }
           <OutlinedInput>
             <PasswordRounded />
             <Input
               placeholder="Password"
+              name="password"
               type={showPassword ? "text" : "password"}
+              value={formData.password}
+              onChange={handleInputChange}
             />
             {showPassword ? (
               <Visibility
@@ -131,8 +304,19 @@ const SignUp = () => {
               />
             )}
           </OutlinedInput>
+          {
+            // Show error message if there is one
+            errorMessage?.password && (
+              <Error style={{ color: "red" }}>{errorMessage.password}</Error>
+            )
+          }
         </Form>
-        <Button>Create Account</Button>
+        <Button
+          onClick={(e) => handleSubmit(e)}
+          buttonDisabled={buttonDisabled}
+        >
+          Create Account
+        </Button>
         <Text>
           Already have an account? <TextButton>SignIn</TextButton>
         </Text>
