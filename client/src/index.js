b7e0739e0dcec100cd30758e770acb033919afe1
rishavchanda <rishavchanda0@gmail.com>
2023-06-07 01:56:37 +0530

    Added and mades some changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
-import { store, persistor } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
