b2c4e9e204984b783f38bfc8baf5223dad09ffd3
rishavchanda <rishavchanda0@gmail.com>
2023-06-08 00:53:08 +0530

    removed strict mode

--- client/src/index.js
+++ client/src/index.js
@@ -8,11 +8,9 @@ import { store, persistor } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
