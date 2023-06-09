08f9da869addfff26060025ce1cd4575edb9d4a7
rishavchanda <rishavchanda0@gmail.com>
2023-06-09 16:22:02 +0530

    Added path modified logo and avatar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import styled from "styled-components";
-import { Avatar } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Avatar, IconButton } from "@mui/material";
+import { Link, useLocation } from "react-router-dom";
+import { MenuRounded } from "@mui/icons-material";
 import DropdownIcon from "@mui/icons-material/ArrowDropDown";
 import { useSelector } from "react-redux";
 import UserProfile from "./Profile";
@@ -17,14 +18,34 @@ const Container = styled.div`
   padding: 18px 50px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
   @media only screen and (max-width: 600px) {
-    padding: 18px 20px;
+    padding: 16px 14px;
+  }
+`;
+
+const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  @media only screen and (max-width: 600px) {
+    gap: 8px;
+  }
+`;
+
+const MenuIcon = styled(IconButton)`
+  color: ${({ theme }) => theme.text_primary} !important;
+  display: none !important;
+  @media (max-width: 1100px) {
+    display: flex !important;
   }
 `;
 
 const LogoText = styled(Link)`
-  font-size: 28px;
+  @media (max-width: 1100px) {
+    display: flex;
+    font-size: 20px;
+  }
+  display: none;
   font-weight: bold;
-  display: flex;
   align-items: center;
   text-transform: uppercase;
   background: linear-gradient(
@@ -36,17 +57,23 @@ const LogoText = styled(Link)`
   -webkit-text-fill-color: transparent;
   background-clip: text;
   text-fill-color: transparent;
-  @media only screen and (max-width: 600px) {
-    font-size: 24px;
-  }
+  font-size: 24px;
 `;
 
 const LogoImg = styled.img`
-  height: 36px;
+  display: none;
+  height: 22px;
   margin-right: 10px;
-  @media only screen and (max-width: 600px) {
-    width: 30px;
-    height: 30px;
+  @media (max-width: 1100px) {
+    display: block;
+  }
+`;
+
+const Path = styled.div`
+  font-size: 22px;
+  font-weight: 600;
+  @media (max-width: 1100px) {
+    display: none;
   }
 `;
 
@@ -66,8 +93,9 @@ const UserName = styled.span`
   }
 `;
 
-const Navbar = () => {
+const Navbar = ({ setMenuOpen, menuOpen }) => {
   const { currentUser } = useSelector((state) => state.user);
+  const location = useLocation();
 
   // Open the account dialog
   const [anchorEl, setAnchorEl] = useState(null);
@@ -81,14 +109,53 @@ const Navbar = () => {
     setAnchorEl(null);
   };
 
+  // create a color code based on user name
+  const generateColor = (name) => {
+    const nameHash = name
+      .toLowerCase()
+      .split("")
+      .reduce((hash, char) => {
+        const charCode = char.charCodeAt(0);
+        return (((hash % 65536) * 65536) % 2147483648) + charCode;
+      }, 0);
+
+    const hue = nameHash % 360;
+    const saturation = 75; // Random value between 25 and 100
+    const lightness = 40; // Random value between 20 and 80
+
+    return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+  };
+
+  // get the main path from the location
+  let path = location.pathname.split("/")[1];
+  if (path === "") path = "Dashboard";
+  else if (path === "profile") path = "Profile";
+  else if (path === "employees") path = "Employees";
+  else if (path === "tasks") path = "Tasks";
+  else path = "";
+
   return (
     <Container>
-      <LogoText to="/">
-        <LogoImg src={Logo} />
-        Trackify
-      </LogoText>
+      <Flex>
+        <MenuIcon onClick={() => setMenuOpen(!menuOpen)}>
+          <MenuRounded sx={{ fontSize: "30px" }} />
+        </MenuIcon>
+        <Path>{path}</Path>
+        <LogoText to="/">
+          <LogoImg src={Logo} />
+          Trackify
+        </LogoText>
+      </Flex>
       <User aria-describedby={id} onClick={handleClick}>
-        <Avatar src={currentUser?.img} />
+        <Avatar
+          src={currentUser?.img}
+          style={{
+            fontSize: "16px",
+            background: generateColor(currentUser?.username)
+          }}
+        >
+          {currentUser?.username[0]}
+        </Avatar>
         <UserName>{currentUser?.username}</UserName>
         <DropdownIcon />
       </User>
