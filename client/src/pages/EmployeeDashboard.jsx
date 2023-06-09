4a264d189217dc968c7849e89c33271a2a5986bb
rishavchanda <rishavchanda0@gmail.com>
2023-06-09 16:24:47 +0530

    Basic page

--- client/src/pages/EmployeeDashboard.jsx
+++ client/src/pages/EmployeeDashboard.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+  padding: 20px 30px;
+  padding-bottom: 200px;
+  height: 100%;
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  @media (max-width: 768px) {
+    padding: 6px 10px;
+  }
+`;
 const EmployeeDashboard = () => {
-  return <div>Employee Dashboard</div>;
+  return <Container>Employee Dashboard</Container>;
 };
 
 export default EmployeeDashboard;
