18e5a5d42fd4d4edc658b68681677ba101fc8257
rishavchanda <rishavchanda0@gmail.com>
2023-06-17 15:08:43 +0530

    added routes for employee details

--- server/routes/admins.js
+++ server/routes/admins.js
@@ -1,11 +1,16 @@
 /* eslint-disable import/extensions */
 import express from "express";
-import { EmployeeRegister, getAllEmployee } from "../controllers/admin.js";
+import {
+  EmployeeRegister,
+  getAllEmployee,
+  getEmployee
+} from "../controllers/admin.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
 router.post("/employeeregister", verifyToken, EmployeeRegister);
 router.get("/getAllEmployees", verifyToken, getAllEmployee);
+router.get("/getEmployee/:employeeId", verifyToken, getEmployee);
 
 export default router;
