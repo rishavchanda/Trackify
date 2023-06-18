f2a4d62b08d627386d2c727b00f3750c25ad52ef
rishavchanda <rishavchanda0@gmail.com>
2023-06-18 12:09:36 +0530

    updated loader

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -7,15 +7,29 @@ const Container = styled.div`
   gap: 16px;
   justify-content: center;
   align-items: center;
-  height: 70vh;
+  height: 100%;
+  width: 100%;
   color: ${({ theme }) => theme.text_primary};
 `;
 
+const Background = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 16px;
+  border-radius: 50%;
+  background-color: ${({ theme }) => theme.card};
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+`;
 const Loader = () => {
   return (
     <Container>
-      <CircularProgress sx={{ color: "inherit" }} />
-      Loading
+      <Background>
+        <CircularProgress
+          sx={{ color: "inherit" }}
+          style={{ width: "32px", height: "32px" }}
+        />
+      </Background>
     </Container>
   );
 };
