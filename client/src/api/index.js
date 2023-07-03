bd43be02574ae8531066f8b0b6876824d2494187
rishavchanda <rishavchanda0@gmail.com>
2023-07-03 16:41:24 +0530

    added api url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `${process.env.REACT_APP_SERVER_URL}/api/`
+  baseURL: "https://trackify-server.duckdns.org/api/"
 });
 
 export const AdminRegister = async (data) =>
